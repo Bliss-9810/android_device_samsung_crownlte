231e753821c9a1ee95a4be196e6e06857dc97283
Faiz Faadhillah <faiz.faadhillah@gmail.com>
2022-08-14 20:28:09 +0700

    minimalist Era.

--- xdroid_crownlte.mk
+++ xdroid_crownlte.mk
@@ -28,11 +28,13 @@ TARGET_SCREEN_HEIGHT := 2960
 TARGET_SCREEN_WIDTH := 1440
 TARGET_BOOT_ANIMATION_RES := 1440
 
-## Inherit some common AOSP stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+## Inherit some common xdroid stuff
+$(call inherit-product, vendor/xdroid/config/common.mk)
+XDROID_BOOT := 1440
+XDROID_MAINTAINER := Faiz
 
 ## Device identifier, this must come after all inclusions
-PRODUCT_NAME := aosp_crownlte
+PRODUCT_NAME := xdroid_crownlte
 PRODUCT_DEVICE := crownlte
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-N960F
